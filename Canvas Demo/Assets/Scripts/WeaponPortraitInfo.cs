b1492db76130489d0014ac3afa719536ad51ca34
emma brown <78447217+rhit-browneg@users.noreply.github.com>
2024-03-28 20:33:51 -0400

    fixed the sprite issue

--- Canvas Demo/Assets/Scripts/WeaponPortraitInfo.cs
+++ Canvas Demo/Assets/Scripts/WeaponPortraitInfo.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponPortraitInfo : MonoBehaviour
-{private SpriteRenderer spriteRenderer;
-public Sprite[] weaponSprites;
- 
- 
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] weaponSprites;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,26 +17,33 @@ public Sprite[] weaponSprites;
     // Update is called once per frame
     void Update()
     {
-        switch(GameManager.Instance.GetCurrentWeapon()){
+        Debug.Log("CURRENT WEAPON:  " + GameManager.Instance.GetCurrentWeapon());
+        int currentWeapon = GameManager.Instance.GetCurrentWeapon();
+        switch (currentWeapon)
+        {
             case 1:
-            Debug.Log("rendering weapon");
+                Debug.Log("Rendering weapon 1");
                 spriteRenderer.sprite = weaponSprites[0];
                 break;
             case 2:
-            spriteRenderer.sprite = weaponSprites[1];
+                Debug.Log("Rendering weapon 2");
+                spriteRenderer.sprite = weaponSprites[1];
                 break;
             case 3:
-            spriteRenderer.sprite = weaponSprites[2];
+                Debug.Log("Rendering weapon 3");
+                spriteRenderer.sprite = weaponSprites[2];
                 break;
             case 4:
-            spriteRenderer.sprite = weaponSprites[3];
+                Debug.Log("Rendering weapon 4");
+                spriteRenderer.sprite = weaponSprites[3];
                 break;
             case 5:
-            spriteRenderer.sprite = weaponSprites[4];
+                Debug.Log("Rendering weapon 5");
+                spriteRenderer.sprite = weaponSprites[4];
                 break;
-
+            default:
+                Debug.LogError("Nope!");
+                break;
         }
-        }
-    
-
+    }
 }
